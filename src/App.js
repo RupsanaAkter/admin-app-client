9f3149872942fea6faf1c5b079ddf7ae3455c331
R7558 <rupsanaworld1004@gmail.co>
2023-05-25 13:23:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login/Login';
+import ResetPassword from './pages/ResetPassword/ResetPassword';
+import ForgetPassword from './pages/ForgetPassword/ForgetPassword';
+import MainLayout from './components/Mainlayout/MainLayout';
+import Dashboard from './pages/Dashboard/Dashboard';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/forgot-password" element={<ForgetPassword />} />
+      <Route path="/admin" element={<MainLayout />}>
+        <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/ResetPassword/ResetPassword.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResetPassword = () => {
+    return (
+        <div>
+            This is reset
+        </div>
+    );
+};
+
+export default ResetPassword;
