34e0fbe3f065904cd80607d431fec14bb55c22b3
R7558 <rupsanaworld1004@gmail.co>
2023-05-26 13:57:47 +0600

    added next all

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MainLayout from './components/Mainlayout/MainLayout';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Addblog from './pages/Addblog/Addblog';
 import Customers from './pages/Customers/Customers';
+import Enquries from './pages/Enquries/Enquries';
 function App() {
   return (
     <Router>
@@ -20,10 +21,10 @@ function App() {
         
 
 
-        {/* <Route path="enquiries" element={<Enquiries />} />
-          <Route path="enquiries/:id" element={<ViewEnq />} />
+          <Route path="enquiries" element={<Enquries />} />
+          {/* <Route path="enquiries/:id" element={<ViewEnq />} />
           <Route path="blog-list" element={<Bloglist />} /> */}
-          <Route path="blog" element={<Addblog />} />
+          <Route path="blog" element={<Addblog />} /> 
           <Route path="blog/:id" element={<Addblog />} />
            {/* <Route path="coupon-list" element={<Couponlist />} />
           <Route path="coupon" element={<AddCoupon />} />
---
+++ src/features/enquiry/EnquirySlice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './EnquiryService'
+const EnquirySlice = () => {
+    return (
+        <div>
+            <h1>this is slice</h1>
+        </div>
+    );
+};
+
+export default EnquirySlice;
